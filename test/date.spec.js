310d96b7b1499095c517e0d1aa9cbbd057dd3c85
Andrew McLagan <andrew@ethicaljobs.com.au>
2018-06-14 12:38:39 +1000

    adding iso string function and tests

---
+++ test/date.spec.js
@@ -0,0 +1,28 @@
+import { Dates } from '../src';
+
+describe('toISOString function', () => {
+
+  test('it always returns a string', () => {
+    expect(
+      typeof Dates.toISOString(new Date)
+    ).toBe('string');
+  });
+
+  test('it formats string into ISO8601 complient date', () => {
+    expect(
+      Dates.toISOString(new Date('05 October 2011 14:48 UTC'))
+    ).toBe('2011-10-05T14:48:00.000Z');
+  });
+
+  test('it also handles string input', () => {
+    expect(
+      Dates.toISOString('2011/10/05 14:48 UTC')
+    ).toBe('2011-10-05T14:48:00.000Z');
+  });
+
+  test('it also handles timestamp input', () => {
+    expect(
+      Dates.toISOString(1526609035000)
+    ).toBe('2018-05-18T02:03:55.000Z');
+  });
+});
